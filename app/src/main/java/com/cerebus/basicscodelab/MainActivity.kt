1e8b46807acba3665498b291826852f0fba5f7c1
Anastasia Drogunova <a.drogunova@start.ru>
2023-08-11 16:26:42 +0300

    created 6 items

--- app/src/main/java/com/cerebus/basicscodelab/MainActivity.kt
+++ app/src/main/java/com/cerebus/basicscodelab/MainActivity.kt
@@ -3,13 +3,19 @@ package com.cerebus.basicscodelab
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.ElevatedButton
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import com.cerebus.basicscodelab.ui.theme.BasicsCodelabTheme
 
 class MainActivity : ComponentActivity() {
@@ -18,26 +24,56 @@ class MainActivity : ComponentActivity() {
         setContent {
             BasicsCodelabTheme {
                 // A surface container using the 'background' color from the theme
-                Surface(modifier = Modifier.fillMaxSize(), color = MaterialTheme.colorScheme.background) {
-                    Greeting("Android")
-                }
+                MyApp(modifier = Modifier.fillMaxSize())
             }
         }
     }
 }
 
 @Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
+fun Greeting(name: String) {
+    Surface(
+        color = MaterialTheme.colorScheme.primary,
+        modifier = Modifier.padding(vertical = 4.dp, horizontal = 8.dp)
     )
+    {
+        Row(modifier = Modifier
+            .padding(24.dp)) {
+            Column(
+                modifier = Modifier.weight(1f)
+            ) {
+                Text(text = "Hello, ")
+                Text(text = name)
+            }
+            ElevatedButton(onClick = { /*TODO*/ }, modifier = Modifier.weight(1f)) {
+                Text(text = "Show more")
+            }
+        }
+    }
 }
 
-@Preview(showBackground = true)
+
+
+@Preview(showBackground = true, widthDp = 320)
 @Composable
-fun GreetingPreview() {
+fun DefaultPreview() {
     BasicsCodelabTheme {
-        Greeting("Android")
+        // A surface container using the 'background' color from the theme
+        MyApp(modifier = Modifier.fillMaxSize())
+    }
+}
+
+
+@Composable
+private fun MyApp(modifier: Modifier = Modifier,
+    names: List<String> = listOf("World", "Compose")
+) {
+    Column(modifier = modifier.padding(vertical = 4.dp)) {
+        for (name in names) {
+            Row(modifier = Modifier.fillMaxWidth()) {
+                Greeting(name = name)
+            }
+
+        }
     }
 }
